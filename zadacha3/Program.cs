8546e42afd74a3caf287f5f96c9d1b8bcb597cc9
Olga <vvs.olga@yandex.ru>
2022-11-27 19:59:18 +0400

    задача3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введи цифру, обозначающую день недели от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (day == 6 || day== 7) {
+  Console.WriteLine("(этот день выходной) -> да");
+  }
+  else if (day < 1 || day > 7) {
+    Console.WriteLine("ошибка, ввидите другое число");
+  }
+  else Console.WriteLine("(этот день выходной) -> нет");
+}
+
+CheckingTheDayOfTheWeek(day);
